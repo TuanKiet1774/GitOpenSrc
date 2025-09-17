ab858f16e1113c3825c190028ae1b96783fa94ee
Phạm Tuấn Kiệt <167632480+TuanKiet1774@users.noreply.github.com>
2025-09-18 00:05:48 +0700

    Bài 1 Trang 78 Chương 2

---
+++ LearnPHP/Bai1_Page78.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài tập 1 - Trang 78 Slide Chương 2</title>
+</head>
+
+<body>
+    <div>
+        <h1><strong>Bài 1:</strong> Viết 1 trang web nhận một giá trị ngẫu nhiên là số tự nhiên N có giá trị từ 1 → 100. Hãy xuất ra trình duyệt số chẵn nằm trong khoảng 1 -> N đó</h1>
+
+    </div>
+</body>
+
+</html>
